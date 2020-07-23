6b08303533b2f666182e434b2a9541f37795494e
Darijo <darijonanetu@gmail.com>
2020-07-23 12:23:45 +0200

    Sveobuhvatni program

--- Program1/Program1/Program1.cpp
+++ Program1/Program1/Program1.cpp
@@ -1,17 +1,24 @@
-// Program1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include<stdio.h>
 #include <iostream>
 #include<stdbool.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv [])
 {
-    int a, b, c;
+    int number_of_arguments = argc;
+    char *First_arg = argv[0];       /* This is the program name. */
+    char *Second_arg = argv[1];      // Prvi argument koji si mu poslao prilikom pokretanja programa
+                                     // Za pokretanje iz powershell-a  | >Program1.exe Argument1 Argument2
+
+    printf("%s \n", First_arg);      /*Will print out the program path*/
+    printf("%s \n", Second_arg);
+
+    int a, b;
+    float c = 3.126;                      printf(" %.2f ", c); // ZAOKRUZUJE dvije decimale - ispisuje 3.13
 
     char My_character = 'b';
-    string My_string = "Hello bitches";
+    string My_string = "Hello bitches";   cout << My_string[1];
 
     bool as = true;      // With #include<stdbool.h>
     bool es = false;
@@ -19,10 +26,8 @@ int main()
     enum Color { red, green, blue };
     enum Color Boja_auspuha = red;
 
-    cout << My_string[1];
-
     scanf_s("%d", &a);
-    printf("\n%d\n", a);
+    printf("\n %d \n", a);
 
     cin >> b;
     cout << "\n" << b << "\n";
@@ -30,14 +35,4 @@ int main()
 
 
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
