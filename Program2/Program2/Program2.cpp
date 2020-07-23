1c18b922aec35480539510a74ff74c46e3e7da7e
Darijo <darijonanetu@gmail.com>
2020-07-23 11:03:12 +0200

    Dodajem Program2

---
+++ Program2/Program2/Program2.cpp
@@ -0,0 +1,16 @@
+// Program2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<stdio.h>
+#include<stdbool.h>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b, c;
+    bool as = true;
+    bool es = false;
+
+    return 0;
+
+}
